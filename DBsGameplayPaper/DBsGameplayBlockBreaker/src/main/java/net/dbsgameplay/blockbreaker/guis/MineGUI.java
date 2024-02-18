eccadf92bacbcc0323545c77b0c4a55aa28f1cb3
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-02-18 19:02:07 +0100

    Mine Command Erste 36 Ebenen

--- DBsGameplayPaper/DBsGameplayBlockBreaker/src/main/java/net/dbsgameplay/blockbreaker/guis/MineGUI.java
+++ DBsGameplayPaper/DBsGameplayBlockBreaker/src/main/java/net/dbsgameplay/blockbreaker/guis/MineGUI.java
@@ -8,49 +8,62 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.HashMap;
+
 public class MineGUI {
   public MineGUI(Player player) {
-    Inventory inventory = Bukkit.createInventory(null, 27, ChatColor.GRAY + "Mine auswählen");
-    ItemStack mine1 = new ItemStack(Material.STONE, 1);
-    ItemMeta meta1 = mine1.getItemMeta();
-    meta1.setDisplayName("Mine 1");
-    mine1.setItemMeta(meta1);
-    inventory.setItem(0, mine1);
-    ItemStack mine2 = new ItemStack(Material.GRANITE, 1);
-    ItemMeta meta2 = mine2.getItemMeta();
-    meta2.setDisplayName("Mine 2");
-    mine2.setItemMeta(meta2);
-    inventory.setItem(1, mine2);
-    ItemStack mine3 = new ItemStack(Material.POLISHED_GRANITE, 1);
-    ItemMeta meta3 = mine3.getItemMeta();
-    meta3.setDisplayName("Mine 3");
-    mine3.setItemMeta(meta3);
-    inventory.setItem(2, mine3);
-    ItemStack mine4 = new ItemStack(Material.DIORITE, 1);
-    ItemMeta meta4 = mine4.getItemMeta();
-    meta4.setDisplayName("Mine 4");
-    mine4.setItemMeta(meta4);
-    inventory.setItem(3, mine4);
-    ItemStack mine5 = new ItemStack(Material.POLISHED_DIORITE, 1);
-    ItemMeta meta5 = mine5.getItemMeta();
-    meta5.setDisplayName("Mine 5");
-    mine5.setItemMeta(meta5);
-    inventory.setItem(4, mine5);
-    ItemStack mine6 = new ItemStack(Material.ANDESITE, 1);
-    ItemMeta meta6 = mine6.getItemMeta();
-    meta6.setDisplayName("Mine 6");
-    mine6.setItemMeta(meta6);
-    inventory.setItem(5, mine6);
-    ItemStack mine7 = new ItemStack(Material.POLISHED_ANDESITE, 1);
-    ItemMeta meta7 = mine7.getItemMeta();
-    meta7.setDisplayName("Mine 7");
-    mine7.setItemMeta(meta7);
-    inventory.setItem(6, mine7);
-    ItemStack mine8 = new ItemStack(Material.GRASS_BLOCK, 1);
-    ItemMeta meta8 = mine8.getItemMeta();
-    meta8.setDisplayName("Mine 8");
-    mine8.setItemMeta(meta8);
-    inventory.setItem(7, mine8);
+    Inventory inventory = Bukkit.createInventory(null, 45, ChatColor.GRAY + "Mine auswählen");
+
+    HashMap<String, Material> ebenenMap = new HashMap<>();
+    ebenenMap.put("Ebene 1", Material.STONE);
+    ebenenMap.put("Ebene 2", Material.GRANITE);
+    ebenenMap.put("Ebene 3", Material.POLISHED_GRANITE);
+    ebenenMap.put("Ebene 4", Material.DIORITE);
+    ebenenMap.put("Ebene 5", Material.POLISHED_DIORITE);
+    ebenenMap.put("Ebene 6", Material.ANDESITE);
+    ebenenMap.put("Ebene 7", Material.POLISHED_ANDESITE);
+    ebenenMap.put("Ebene 8", Material.GRASS_BLOCK);
+    ebenenMap.put("Ebene 9", Material.DIRT);
+    ebenenMap.put("Ebene 10", Material.COARSE_DIRT);
+    ebenenMap.put("Ebene 11", Material.PODZOL);
+    ebenenMap.put("Ebene 12", Material.COBBLESTONE);
+    ebenenMap.put("Ebene 13", Material.OAK_PLANKS);
+    ebenenMap.put("Ebene 14", Material.SPRUCE_PLANKS);
+    ebenenMap.put("Ebene 15", Material.BIRCH_PLANKS);
+    ebenenMap.put("Ebene 16", Material.JUNGLE_PLANKS);
+    ebenenMap.put("Ebene 17", Material.ACACIA_PLANKS);
+    ebenenMap.put("Ebene 18", Material.DARK_OAK_PLANKS);
+    ebenenMap.put("Ebene 19", Material.SAND);
+    ebenenMap.put("Ebene 20", Material.RED_SAND);
+    ebenenMap.put("Ebene 21", Material.GRAVEL);
+    ebenenMap.put("Ebene 22", Material.GOLD_ORE);
+    ebenenMap.put("Ebene 23", Material.IRON_ORE);
+    ebenenMap.put("Ebene 24", Material.COAL_ORE);
+    ebenenMap.put("Ebene 25", Material.OAK_WOOD);
+    ebenenMap.put("Ebene 26", Material.SPRUCE_WOOD);
+    ebenenMap.put("Ebene 27", Material.BIRCH_WOOD);
+    ebenenMap.put("Ebene 28", Material.JUNGLE_WOOD);
+    ebenenMap.put("Ebene 29", Material.OAK_LEAVES);
+    ebenenMap.put("Ebene 30", Material.SPRUCE_LEAVES);
+    ebenenMap.put("Ebene 31", Material.BIRCH_LEAVES);
+    ebenenMap.put("Ebene 32", Material.JUNGLE_LEAVES);
+    ebenenMap.put("Ebene 33", Material.SPONGE);
+    ebenenMap.put("Ebene 34", Material.WET_SPONGE);
+    ebenenMap.put("Ebene 35", Material.GLASS);
+    ebenenMap.put("Ebene 36", Material.LAPIS_ORE);
+
+    for (int i = 0; i < 36; i++) {
+      String ebenenKey = "Ebene " + (i + 1);
+      Material material = ebenenMap.get(ebenenKey);
+      ItemStack item = new ItemStack(material, 1);
+      ItemMeta meta = item.getItemMeta();
+      if (meta != null) {
+        meta.setDisplayName(ebenenKey.substring(0, 1).toUpperCase() + ebenenKey.substring(1));
+        item.setItemMeta(meta);
+        inventory.setItem(i, item);
+      }
+    }
+
     player.openInventory(inventory);
   }
 }
